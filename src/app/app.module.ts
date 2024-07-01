3f6ed710fc90b8e0fe54df7a61347712221a29c5
Markfady <markfady9@gmail.com>
2024-07-01 13:19:24 +0300

    setup paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
+import { AllProductsComponent } from './products/components/all-products/all-products.component';
+import { ProductsDetailsComponent } from './products/components/products-details/products-details.component';
+import { CartComponent } from './cart/components/cart/cart.component';
+import { HomeComponent } from './home/home.component';
 
+const routes:Routes=[
+  {path:'',component:HomeComponent,pathMatch:'full'},
+  {path:'products',component:AllProductsComponent},
+  {path:'details',component:ProductsDetailsComponent},
+  {path:'cart',component:CartComponent},
+  {path:'**',redirectTo:'products',pathMatch:'full'}
+]
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([]),
+    RouterModule.forRoot(routes),
     SharedModule
   ],
   providers:[],
