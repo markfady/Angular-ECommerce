e4e3bd0d1785a3ea75b90855cbdbb6d19ab4d170
Markfady <markfady9@gmail.com>
2024-07-30 15:46:31 +0300

    Boolean value to use spinner

--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -12,7 +12,8 @@ import { Product } from '../../productsInterface';
 export class AllProductsComponent implements OnInit {
   
   products: Product[] = [];
-  categories:any[]=[]
+  categories:any[]=[];
+  loading:boolean=true
 
   constructor(private service: ProductsService) { }
 
@@ -23,31 +24,39 @@ export class AllProductsComponent implements OnInit {
 
   //subscribe to get all products
   getProducts(){
-
+    this.loading=true
     this.service.getAllProducts().subscribe((res:any)=>{
     this.products=res
+    this.loading=false
     },error=>{
+      this.loading=true
       console.log(error)
     })
 
   }
   //subscribe to get the category names and loop on it inside the select statement
   filteredCategories(){
+    this.loading=true
     this.service.getCategories().subscribe((res:any)=>{
       this.categories=res
+      this.loading=false
       console.log(res)
     },error=>{
+      this.loading=true
       console.log(error)
     })
   }
   //know what category user selected
   filterProductsByCategory(event:any){
+    this.loading=true
   let value=event.target.value
   value==="All"?this.getProducts() :  this.getProductOfCategory(value)
   }
   //pass the selected category to the service
   getProductOfCategory(category:string){
+    this.loading=true
     this.service.getProductsOfCategory(category).subscribe(res=>{
+      this.loading=false
       this.products=res
     })
   }
