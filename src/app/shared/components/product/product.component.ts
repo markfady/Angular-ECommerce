9b0ecde9d6e45be2ddd9159da6f6a439f67afe8c
Markfady <markfady9@gmail.com>
2024-07-31 17:59:06 +0300

    send quantity value with item object

--- src/app/shared/components/product/product.component.ts
+++ src/app/shared/components/product/product.component.ts
@@ -10,12 +10,14 @@ export class ProductComponent implements OnInit {
 @Input () item:any={}
 //sending data from child to parent () using @output()
 @Output() data=new EventEmitter()
+addItem:boolean=false
+amount:number=0;
   constructor() { }
 
   ngOnInit(): void {
   }
   //now we can receive the data from child to this function
-  add(){
-    this.data.emit(this.item)
+  add(){ //make object item composed of item key , quantity key.
+    this.data.emit({item:this.item,quantity:this.amount})
   }
 }
