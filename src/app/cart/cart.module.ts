7c83941f1591a929562ffa7f4bc1a6d23c4ee653
Markfady <markfady9@gmail.com>
2024-08-01 16:31:20 +0300

    formModule to use ngModel

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './components/cart/cart.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +12,8 @@ import { BrowserModule } from '@angular/platform-browser';
   ],
   imports: [
     CommonModule,
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ]
 })
 export class CartModule { }
