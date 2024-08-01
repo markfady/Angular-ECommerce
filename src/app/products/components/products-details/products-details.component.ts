88960ea82785da42f114f6a388b8595bd26531b0
Markfady <markfady9@gmail.com>
2024-08-01 11:59:07 +0300

    setup product detailed information

--- src/app/products/components/products-details/products-details.component.ts
+++ src/app/products/components/products-details/products-details.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../service/products.service';
+import { Product } from '../../productsInterface';
 
 @Component({
   selector: 'pm-products-details',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-details.component.css']
 })
 export class ProductsDetailsComponent implements OnInit {
-
-  constructor() { }
+  id:any
+  productDetails!:Product; // Changed from Product[] to Product
+  constructor(private route:ActivatedRoute,private service:ProductsService) {
+      this.id=route.snapshot.paramMap.get('id');
+      console.log(this.id)
+   }
 
   ngOnInit(): void {
+    this.getProductDetails(this.id)
   }
+//response from getProductByID is a single Product object, not an array. Therefore, handle the response accordingly
 
+ getProductDetails(id: any): void {
+  this.service.getProductByID(id).subscribe((res:Product) => {
+    this.productDetails = res; // Directly assign the response object
+    console.log(res);
+  });
 }
+}
+
