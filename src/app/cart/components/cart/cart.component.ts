322ffca823e6bd1a5e785104e75c11542e73a9ad
Markfady <markfady9@gmail.com>
2024-08-01 16:30:42 +0300

    list item in cart page , calculate total

--- src/app/cart/components/cart/cart.component.ts
+++ src/app/cart/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartProduct, Product } from 'src/app/products/productsInterface';
 
 @Component({
   selector: 'pm-cart',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
+  cartProducts:CartProduct[]=[]
+  total:number=0
   constructor() { }
 
   ngOnInit(): void {
+    this.getCartProducts()
   }
-
+getCartProducts(){
+  if("cart" in localStorage){
+      this.cartProducts = JSON.parse(localStorage.getItem("cart")!);
+      this.getCartTotal()
+      console.log(this.cartProducts)
+  }
+}
+  getCartTotal(){
+    this.total=0;
+    for(let x in this.cartProducts){
+      this.total+=this.cartProducts[x].quantity*this.cartProducts[x].item.price
+    }
+  }
 }
